67e78fafc143c8ad36b4a5a73abe4d502f2e5ebf
Harish Bhati <hbhati@ex2india.local>
2021-08-30 10:14:50 +0530

    project setup files

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="wr__flex">
+    <div class="wr__flex-2">Item 1</div>
+    <div class="wr__flex-2">Item 2</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+<style lang="scss">
+.wr__flex{
+    @include flex(flex,center,center);
+}
+</style>
